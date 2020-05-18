b17b5d06c076d695aaf9762a0a8a81a6d83b2504
vikas <v8kgsm@gmail.com>
2020-05-18 09:17:15 +0530

    Closures.

---
+++ Lecture51/js/script.js
@@ -0,0 +1,21 @@
+// Closures
+function makeMultiplier (multiplier) {
+  // var multiplier = 2;
+  function b() {
+    console.log("Multiplier is: " + multiplier);
+  }
+  b();
+
+  return multiplier * x;
+
+
+  // return (
+  //     function (x) {
+  //       return multiplier * x;
+  //     }
+  //
+  //   );
+}
+
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(10)); // its own exec env
