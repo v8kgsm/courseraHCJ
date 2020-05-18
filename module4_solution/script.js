4b3640503c3050992be3cee0df0e1bb7fc33129d
vikas <v8kgsm@gmail.com>
2020-05-18 23:50:07 +0530

    Module 4 solution.

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Joe", "Jim"];
+    var names = ["Yaakov", "John","Frank", "Larry", "Jen", "Joe", "Paul",  "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
